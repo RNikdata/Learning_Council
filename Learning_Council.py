8dc555356c0597ac69b2ec1b7e2113b1031fcdbe
RNikdata <162301716+RNikdata@users.noreply.github.com>
2025-11-06 08:46:53 +0530

    Update Learning_Council.py

--- Learning_Council.py
+++ Learning_Council.py
@@ -1,12 +1,23 @@
 import streamlit as st
 import pandas as pd
- 
+
+
+# --- Google Sheets setup ---
+# --- Connect to Google Sheets using Streamlit secrets ---
+service_account_info = st.secrets["google_service_account"]
+scopes = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
+credentials = Credentials.from_service_account_info(service_account_info, scopes=scopes)
+gc = gspread.authorize(credentials)
+
+# # Your Google Sheet ID and worksheet name
+SHEET_ID = "1tg4xQzY-zVu3_8QJWD6NNbrxUSbTCJYVABpCeVmCnVg"
+sheet1 = "Certifications & Skill Gap Tracker"
+
+data_sheet = gc.open_by_key(SHEET_ID).worksheet(sheet1)
+# --- Load data from Google Sheets ---
+df = get_as_dataframe(data_sheet, evaluate_formulas=True).dropna(how="all")
+
 st.set_page_config(layout="wide")
- 
-# --- Load Data ---
-df = pd.read_excel(
-    r"C:\Users\Aathikumar.p\Desktop\Learning council\Skill Gap Excel.xlsx"
-)
  
 # --- Tabs ---
 tab1, tab2, tab3 = st.tabs(["🎓✅ Certifications & Skill Gap Tracker", "📘📅 Knowledge Enablement Program Oversight", "🛠️🧩 Prototyping Engagement & Session Planning"])
